2ef6920b29125200d749930c5fb68a69f0a9355f
Daniel Rau <drau88@cox.net>
2021-10-01 09:37:46 -0700

    Sprint challenge started. Completed tasks 1 and 2 successfully.

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,25 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headerDiv = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const h1 = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+
+  headerDiv.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(h1);
+  headerDiv.appendChild(tempSpan);
+
+  dateSpan.textContent = ` ${date} `;
+  h1.textContent = ` ${title} `;
+  tempSpan.textContent = ` ${temp} `;
+
+  return headerDiv;
+
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +39,10 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  const headerToAppend = Header('Finally Cooling Down!', '10/1/2021', '76*' );
+  const entryPoint = document.querySelector(selector);
+  entryPoint.appendChild(headerToAppend);
 }
 
 export { Header, headerAppender }
